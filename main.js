a165e2a1da29b73a8c77f630fbade793af33109b
Shin-sibainu <shincode0712@gmail.com>
2021-12-12 17:41:43 +0900

    setup

---
+++ main.js
@@ -0,0 +1,69 @@
+import * as THREE from "./build/three.module.js";
+import { OrbitControls } from "./controls/OrbitControls.js";
+
+//シーン
+const scene = new THREE.Scene();
+
+//カメラ
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100
+);
+camera.position.set(1, 1, 2);
+
+//レンダラー
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+document.body.appendChild(renderer.domElement);
+
+/**
+ * ジオメトリを作ってみよう。
+ **/
+
+//マテリアル
+const material = new THREE.MeshStandardMaterial({
+  color: "blue",
+});
+
+//マウス操作
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+window.addEventListener("resize", onWindowResize);
+
+const clock = new THREE.Clock();
+
+function animate() {
+  const elapsedTime = clock.getElapsedTime();
+  // console.log(elapsedTime);
+
+  //オブジェクトの回転
+  // sphere.rotation.x = elapsedTime;
+  // plane.rotation.x = elapsedTime;
+  // octahedron.rotation.x = elapsedTime;
+  // torus.rotation.x = elapsedTime;
+
+  // sphere.rotation.y = elapsedTime;
+  // plane.rotation.y = elapsedTime;
+  // octahedron.rotation.y = elapsedTime;
+
+  // torus.rotation.y = elapsedTime;
+
+  controls.update();
+
+  //レンダリング
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
+
+//ブラウザのリサイズに対応
+function onWindowResize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}
+
+animate();
